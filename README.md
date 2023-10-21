6d9683a108edb96c66b8841651fddfe09375ea6c
“Leonardo <leomendesps11@gmail.com>
2023-10-21 20:38:39 -0300

    formulario adicionado

---
+++ README.md
@@ -0,0 +1,50 @@
+# DESAFIO - HTML/CSS/JS INTERMEDIÁRIO
+
+Esta é uma solução para o desafio de [formulário com validação](
+https://www.figma.com/file/zBKnYG9UNdUiIr8ClQTWSG/?node-id=3%3A2
+). Esse desafio visa trabalhar os conhecimentos adquiridos ao longo do curso.
+
+## Índice
+
+- [Visão geral](#visão-geral)
+   - [O desafio](#o-desafio)
+   - [Captura de tela](#captura-de-tela)
+- [Meu processo](#meu-processo)
+   - [Construído com](#construído-com)
+   - [O que aprendi](#o-que-aprendi)
+
+
+## Visão geral
+
+### O desafio
+
+O formulário consiste em:
+
+- Ao clicar para enviar, se caso algum campo não estiver preenchido, a borda do input ficará vermelha e uma mensagem de "campo obrigatório" irá aparecer embaixo do campo que não for preenchido.
+- Se o usuário clicar dentro do campo de input e, logo após clicar fora do mesmo ou então, se o campo for preenchido e o dado digitado for excluído e clicado o click fora, a borda do campo ficará vermelho e a mensagem de "campo obrigatório" será exibida. 
+
+### Captura de tela
+
+![](./designe/Animação.gif)
+
+![](./designe/img(2).jpg)
+
+![](./designe/img(1).jpg)
+
+![](./designe/img(4).jpg)
+
+![](./designe/img(3).jpg)
+
+
+## Meu processo
+
+### Construído com
+
+- Marcação HTML5 semântica
+- Propriedades personalizadas CSS
+- Interação com JavaScript
+
+
+### O que eu aprendi
+
+Esse desafio proporcionou-me desenvolver técnicas práticas de interação com JavaScript, permitindo um melhor entendimento de funções e de como manipular os objetos em geral.
---
+++ src/js/script.js
@@ -0,0 +1,97 @@
+const form = document.getElementById("form");
+const userName = document.getElementById("name");
+const userEmail = document.getElementById("email");
+const telefoneUser = document.getElementById("telefone");
+const messageUser = document.getElementById("mensagem");
+
+const inputText = document.querySelectorAll(".input");
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    verificandoInput();
+    emailInput();
+    telefoneInput();
+    messageInput();
+})
+
+inputText.forEach(input => {
+    input.addEventListener('change', () => {
+        if (input.value !== "") {
+            input.classList.add("campo-preenchido")
+        } else {
+            input.classList.remove("campo-preenchido")
+        }
+    });
+});
+
+function verificandoInput() {
+    const userNameValue = userName.value;
+
+    if (userNameValue == "") {
+        errorInput(userName, "campo obrigatório")
+    } else {
+        const formItem = userName.parentElement;
+        formItem.className = "form-content"
+    }
+}
+
+function emailInput() {
+    const emailValue = userEmail.value;
+
+    if (emailValue == "") {
+        errorInput(userEmail, "campo obrigatório")
+    } else {
+        const formItem = userEmail.parentElement;
+        formItem.className = "form-content"
+    }
+}
+
+function telefoneInput() {
+    const telefoneValue = telefoneUser.value;
+
+    if (telefoneValue == "") {
+        errorInput(telefoneUser, "campo obrigatório")
+    } else {
+        const formItem = telefoneUser.parentElement;
+        formItem.className = "form-content"
+    }
+}
+
+function messageInput() {
+    const messageValue = messageUser.value;
+
+    if (messageValue == "") {
+        errorInput(messageUser, "campo obrigatório")
+    } else {
+        const formItem = messageUser.parentElement;
+        formItem.className = "form-content"
+    }
+}
+
+function errorInput(input, message) {
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("span")
+
+    textMessage.innerText = message;
+
+    formItem.className = "form-content error"
+}
+
+userEmail.addEventListener("blur", () => {
+    emailInput();
+})
+
+
+userName.addEventListener("blur", () => {
+    verificandoInput();
+})
+
+telefoneUser.addEventListener("blur", () => {
+    telefoneInput();
+})
+
+messageUser.addEventListener("blur", () => {
+    messageInput();
+})
