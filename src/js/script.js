883b10d0b0071495cf84219a3850d618b2e64410
“Leonardo <leomendesps11@gmail.com>
2023-10-29 16:31:28 -0300

    Projeto refatorado

--- src/js/script.js
+++ src/js/script.js
@@ -1,97 +1,18 @@
-const form = document.getElementById("form");
-const userName = document.getElementById("name");
-const userEmail = document.getElementById("email");
-const telefoneUser = document.getElementById("telefone");
-const messageUser = document.getElementById("mensagem");
+const dadosFormulario = document.querySelectorAll(".input")
+const clickBotao = document.querySelector('.btn-enviar')
 
-const inputText = document.querySelectorAll(".input");
+clickBotao.addEventListener('click', (event)=> {
+    event.preventDefault()
 
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    verifyInput();
-    emailInput();
-    telefoneInput();
-    messageInput();
-})
-
-inputText.forEach(input => {
-    input.addEventListener('change', () => {
-        if (input.value !== "") {
+    dadosFormulario.forEach((input) => {
+        if (input.value) {
             input.classList.add("campo-preenchido")
+            input.classList.remove("error")
+            input.nextElementSibling.classList.add("campo-obrigatorio")
         } else {
             input.classList.remove("campo-preenchido")
+            input.classList.add("error")
+            input.nextElementSibling.classList.remove("campo-obrigatorio")
         }
     });
-});
-
-function verifyInput() {
-    const userNameValue = userName.value;
-
-    if (userNameValue == "") {
-        errorInput(userName, "campo obrigatório")
-    } else {
-        const formItem = userName.parentElement;
-        formItem.className = "form-content"
-    }
-}
-
-function emailInput() {
-    const emailValue = userEmail.value;
-
-    if (emailValue == "") {
-        errorInput(userEmail, "campo obrigatório")
-    } else {
-        const formItem = userEmail.parentElement;
-        formItem.className = "form-content"
-    }
-}
-
-function telefoneInput() {
-    const telefoneValue = telefoneUser.value;
-
-    if (telefoneValue == "") {
-        errorInput(telefoneUser, "campo obrigatório")
-    } else {
-        const formItem = telefoneUser.parentElement;
-        formItem.className = "form-content"
-    }
-}
-
-function messageInput() {
-    const messageValue = messageUser.value;
-
-    if (messageValue == "") {
-        errorInput(messageUser, "campo obrigatório")
-    } else {
-        const formItem = messageUser.parentElement;
-        formItem.className = "form-content"
-    }
-}
-
-function errorInput(input, message) {
-    const formItem = input.parentElement;
-    const textMessage = formItem.querySelector(".error-message")
-
-    textMessage.innerText = message;
-
-    formItem.className = "form-content error"
-}
-
-userEmail.addEventListener("blur", () => {
-    emailInput();
-})
-
-
-userName.addEventListener("blur", () => {
-    verifyInput();
-})
-
-telefoneUser.addEventListener("blur", () => {
-    telefoneInput();
-})
-
-messageUser.addEventListener("blur", () => {
-    messageInput();
 })
